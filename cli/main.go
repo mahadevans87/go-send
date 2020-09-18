01b890b770c149847c85cdb99af8c5cf1188d220
Mahadevan Sreenivasan <mahadevan_sv@yahoo.com>
2020-09-18 23:09:58 +0530

    - implement peer list api in signal - register a new user and pass the connection info to client

--- cli/main.go
+++ cli/main.go
@@ -1,54 +1,86 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"strings"
+	"time"
 )
 
 const signalBaseURL = "http://localhost:8080"
 
-func registerToken(token string) (*http.Response, error) {
-	resp, err := http.Post(fmt.Sprintf("%s/register?token=%s", signalBaseURL, token), "", strings.NewReader(""))
+// ConnectionInfo can be shared between packages
+type ConnectionInfo struct {
+	Message string `json:"message"`
+	PeerID  int    `json:"peerID"`
+	token   string
+	mode    string
+}
+
+var httpClient = &http.Client{Timeout: 10 * time.Second}
+
+func getJSON(url string, target interface{}) error {
+	r, err := httpClient.Get(url)
+	if err != nil {
+		return err
+	}
+	defer r.Body.Close()
+
+	return json.NewDecoder(r.Body).Decode(target)
+}
+
+func registerToken(token string, connectionInfo *ConnectionInfo) error {
+	resp, err := httpClient.Post(fmt.Sprintf("%s/register?token=%s", signalBaseURL, token), "", strings.NewReader(""))
 	if err != nil {
 		log.Fatal(err)
-		return nil, err
+		return err
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode == http.StatusOK {
-		bodyBytes, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			log.Fatal(err)
+		decodeErr := json.NewDecoder(resp.Body).Decode(connectionInfo)
+		if decodeErr != nil {
+			log.Fatal(decodeErr)
+			return decodeErr
 		}
-		bodyString := string(bodyBytes)
-		log.Println(bodyString)
 	}
-	return resp, err
+	return err
 }
+
+func fetchPeerList(connectionInfo *ConnectionInfo)
 
 func main() {
 	mode := flag.String("mode", "S", "S for send, R for receive. Default is S")
-	//sourcePath := flag.String("src", "/home/mahadevan/test.txt", "Path of the file to send")
+	sourcePath := flag.String("src", "/home/mahadevan/test.txt", "Path of the file to send")
 	token := flag.String("token", "", "Token which the sender and receiver must know (Required)")
 
 	flag.Parse()
 
-	if *token == "" {
+	if *mode != "S" && *mode != "R" || *token == "" {
 		flag.PrintDefaults()
 		os.Exit(1)
 	}
 
-	switch *mode {
-	case "S":
-		registerToken(*token)
-	case "R":
-	default:
-		flag.PrintDefaults()
-		os.Exit(1)
+	var connectionInfo = ConnectionInfo{}
+	if err := registerToken(*token, &connectionInfo); err != nil {
+		log.Fatal(err)
+	} else {
+		// success we have connected
+		log.Println(connectionInfo.PeerID)
+		switch *mode {
+		case "S":
+			client = Client(&connectionInfo)
+			client.SetSourcePath(*sourcePath)
+		case "R":
+		default:
+			flag.PrintDefaults()
+			os.Exit(1)
+		}
+
 	}
+
 }
