fc657813a31a760bbc1e1ab19d09ad21d7acf89c
Mahadevan Sreenivasan <mahadevan_sv@yahoo.com>
2020-11-02 22:53:09 +0530

    - add PeerInfo model to parse the Peers API - Implement fetchPeerList that retrieves the current list of peers

--- cli/main.go
+++ cli/main.go
@@ -21,6 +21,13 @@ type ConnectionInfo struct {
 	mode    string
 }
 
+// PeerInfo Data Model
+type PeerInfo struct {
+	Token    string `json:"token"`
+	ID       string `json:"id"`
+	Messages chan Message
+}
+
 var httpClient = &http.Client{Timeout: 10 * time.Second}
 
 func getJSON(url string, target interface{}) error {
@@ -51,7 +58,31 @@ func registerToken(token string, connectionInfo *ConnectionInfo) error {
 	return err
 }
 
-func fetchPeerList(connectionInfo *ConnectionInfo)
+func fetchPeerList(connectionInfo *ConnectionInfo) error {
+	resp, err := httpClient.Get(fmt.Sprintf("%s/peers?token=%s&id=%s", signalBaseURL, connectionInfo.token, connectionInfo.PeerID))
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode == http.StatusOK {
+		type PeerResponse struct {
+			message string `json:"message"`,
+			peers []PeerInfo `json:"peers"`,
+		}
+		var peerResponse PeerResponse
+
+		decodeErr := json.NewDecoder(resp.Body).Decode(&peerResponse)
+		if decodeErr != nil {
+			log.Fatal(decodeErr)
+			return decodeErr
+		} else {
+			// For now there is only one peer. We need to write a proper client later on
+			*connectionInfo.PeerID = peerResponse.peers[0];
+		}
+	}
+}
 
 func main() {
 	mode := flag.String("mode", "S", "S for send, R for receive. Default is S")
@@ -71,6 +102,14 @@ func main() {
 	} else {
 		// success we have connected
 		log.Println(connectionInfo.PeerID)
+
+		// Fetch PeerInfo
+		if peerInfoFetchErr := fetchPeerList(&connectionInfo); peerInfoFetchErr != nil {
+			log.Fatal(peerInfoFetchErr)
+		} else {
+			
+		}
+
 		switch *mode {
 		case "S":
 			client = Client(&connectionInfo)
